ef920a564fc75b5f8c8505fcbff5c64392cd32f6
Yash Agarwal <agrwal.ysh94@gmail.com>
2017-04-30 22:14:55 +0530

    Generate the points table for all matches: Done

--- player_stats.py
+++ player_stats.py
@@ -11,39 +11,13 @@ def add_player(player_name, player_performance):
                 'fielding':{'catches' : 0, 'stumping' : 0, 'runout_throw': 0, 'runout_catch': 0, 'runout_direct': 0}
                 }))
 
-def get_player_performance_for_match(match_file_name):
+def get_all_player_performance_for_match(match_file_name):
 
-    with open(match_file_name, 'r') as stream:
-        try:
-            data = (yaml.load(stream))
-        except yaml.YAMLError as exc:
-            print(exc)
-
-    #print data
-
-    #for key in data.keys():
-    #    print key
-
-    '''
-    for key in data["innings"][0]["1st innings"].keys():
-        print key
-
-    for key in data["innings"][0]["1st innings"]["deliveries"][0][0.1].keys():
-        print key
-    '''
+    data = get_match_data(match_file_name)
+    if data == -1:
+        return -1
 
     player_performance = dict()
-
-    '''
-    unit = {'batting':
-                    {'runs' : 0, 'balls' : 0, '4s': 0, '6s': 0},
-            'bowling':
-                    {'runs' : 0, 'balls' : 0, 'wickets':0, 'extras':0, '4s': 0, '6s': 0},
-            'fielding':
-                    {'catches' : 0, 'stumping' : 0, 'runout_throw': 0, 'runout_catch': 0, 'runout_direct': 0}
-            }
-    '''
-
     innings1_balls = data["innings"][0]["1st innings"]["deliveries"]
     innings2_balls = data["innings"][1]["2nd innings"]["deliveries"]
     innings1_balls.extend(innings2_balls)
@@ -106,23 +80,55 @@ def get_player_performance_for_match(match_file_name):
 
         player_performance[player_name]["bowling"]["balls"] += 1
 
+    return player_performance
+
+def get_all_player_points_for_match(player_performance):
     player_points = dict()
     import operator
 
     for key in player_performance.keys():
         p = points.player_points(key, player_performance)
-        print key, p
+        #print key, p
         player_points.update({key:p})
         #print player_performance[key]
 
     player_points_sorted = sorted(player_points.items(), key=operator.itemgetter(1))
     player_points_sorted.reverse()
+    return player_points_sorted
+    #print player_points_sorted
 
-    print player_points_sorted
+def save_all_player_points_for_match(match_file_name):
 
-    with open('player_performance.yaml', 'w') as outfile:
-        yaml.dump(player_performance, outfile, default_flow_style=False)
+    player_performance = get_all_player_performance_for_match(match_file_name)
+    if player_performance == -1:
+        return
 
-match_number = 335982
-match_file_name = "data/%d.yaml" % (match_number)
-get_player_performance_for_match(match_file_name)
+    player_points_sorted = get_all_player_points_for_match(player_performance)
+    match_date = get_match_date(match_file_name)
+
+    with open('output/all_player_points_%s.yaml' % (match_date), 'w') as outfile:
+        yaml.dump(player_points_sorted, outfile, default_flow_style=False)
+
+def get_match_date(match_file_name):
+    data = get_match_data(match_file_name)
+    if data != -1:
+        return data["info"]["dates"][0]
+    return ""
+
+def get_match_data(match_file_name):
+    try:
+        with open(match_file_name, 'r') as stream:
+            try:
+                data = (yaml.load(stream))
+            except yaml.YAMLError as exc:
+                print(exc)
+    except:
+        data = -1
+        print('Unable to open file %s' % (match_file_name))
+    finally:
+        return data
+
+for match_number in range(335982,1082626):
+    print match_number, "\r"
+    match_file_name = "data/%d.yaml" % (match_number)
+    save_all_player_points_for_match(match_file_name)
